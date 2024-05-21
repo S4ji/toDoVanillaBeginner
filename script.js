41a3ebb9f1a4d2fa42c06cf5260c26c916c26db0
Yasser El Hafny <yelhafny@antidots-group.com>
2024-05-21 16:32:30 +0200

    feature:Form appear on click and dissppear on submit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // DOM ELEMENT
-
+let formMode = false
 const listContainer = document.getElementById('listContainer')
-const testButton = document.getElementById('test')
+const toggleFormButton = document.getElementById('toggleFormButton')
 const myForm = document.getElementById('myForm')
+const body = document.body
 let inputTitle = document.getElementById('title')
 let inputDescription = document.getElementById('description')
 let inputPriority = document.getElementById('priority')
@@ -25,6 +26,8 @@ function saveToStorage() {
 
 function createTodoDom(toDoObject, index) {
     const toDo = document.createElement('li')
+    toDo.classList.add('bg-yellow-100', 'shadow-lg', 'p-8', 'm-3')
+
     const toDoTitle = document.createElement('h2')
     const toDoId = document.createElement('p')
     const toDoDescription = document.createElement('p')
@@ -32,19 +35,19 @@ function createTodoDom(toDoObject, index) {
     const toDoLasting = document.createElement('p')
     const toDoEditButton = document.createElement('button')
 
-    toDoEditButton.classList.add('toDoEditButton')
+    toDoEditButton.classList.add('toDoEditButton', 'hover:bg-yellow-700', 'm-2')
     toDoEditButton.addEventListener('click', (event) => {
         editTodo(index)
     })
 
     const toDoDeleteButton = document.createElement('button')
-    toDoDeleteButton.classList.add('toDoDeleteButton')
+    toDoDeleteButton.classList.add('toDoDeleteButton', 'hover:bg-yellow-700')
     toDoDeleteButton.addEventListener('click', (event) => {
         deleteTodo(index)
     })
 
     const toDoDoneButton = document.createElement('button')
-    toDoDoneButton.classList.add('toDoDoneButton')
+    toDoDoneButton.classList.add('toDoDoneButton', 'hover:bg-yellow-700')
 
     toDoDoneButton.addEventListener('click', (event) => {
         updateToDo(myForm, index)
@@ -63,11 +66,11 @@ function createTodoDom(toDoObject, index) {
         toDoDoneButton
     )
 
-    toDoId.innerText = toDoObject.id
-    toDoTitle.innerText = toDoObject.title
-    toDoDescription.innerText = toDoObject.description
-    toDoPriority.innerText = toDoObject.priority
-    toDoLasting.innerText = toDoObject.lasting
+    toDoId.innerText = `id : ${toDoObject.id}`
+    toDoTitle.innerText = `title : ${toDoObject.title}`
+    toDoDescription.innerText = `description : ${toDoObject.description}`
+    toDoPriority.innerText = `priority : ${toDoObject.priority}`
+    toDoLasting.innerText = `days lasting before deadline : ${toDoObject.lasting}`
     toDoEditButton.innerText = 'Edit'
     toDoDeleteButton.innerText = 'Delete'
     toDoDoneButton.innerText = 'Done'
@@ -94,6 +97,11 @@ function editTodo(indexToEdit) {
             console.log(toDoList[indexToEdit])
         }
     }
+}
+
+function toggleHiddenClass() {
+    myForm.classList.toggle('hidden')
+    listContainer.classList.toggle('hidden')
 }
 
 // CRUD TODO
@@ -141,8 +149,9 @@ function deleteTodo(indexToDelete) {
 
 // EVENT LISTENER
 
-testButton.addEventListener('click', (event) => {
-    console.log(event)
+toggleFormButton.addEventListener('click', (event) => {
+    toggleHiddenClass()
+    formMode = true
 })
 
 myForm.addEventListener('submit', (e) => {
@@ -150,6 +159,7 @@ myForm.addEventListener('submit', (e) => {
     updateToDoListOnDom()
     createToDoObject(e.target)
     myForm.reset()
+    toggleHiddenClass()
 })
 
 updateToDoListOnDom()
